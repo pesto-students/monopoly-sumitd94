054ac449278cd8252a6a54988c9bf0c7b0fc54fb
Sumit Dhanania <sumitdhanania94@gmail.com>
2020-11-18 02:44:30 +0530

    Monopoly setup

---
+++ src/Components/Players/Players.jsx
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react';
+import { GameContext } from '../../contexts/context';
+import './Players.css';
+
+const Players = () => {
+  const { gameState } = useContext(GameContext);
+  return (
+    <div className="player-details">
+      {gameState.player1.playing ? (
+        <div className="player-1" style={{ borderBottom: gameState.player1.turn ? '3px solid red' : '1px solid #ccc' }}>
+          <div className="player-game__details">
+            <h3 className="player-name">{`${gameState.player1.name}`}</h3>
+            <p id="player__price">{`$${gameState.player1.balance}`}</p>
+          </div>
+        </div>
+      ) : ('')}
+      {gameState.player2.playing ? (
+        <div className="player-2" style={{ borderBottom: gameState.player2.turn ? '3px solid yellow' : '1px solid #ccc' }}>
+          <div className="player-game__details">
+            <h3 className="player-name">{`${gameState.player2.name}`}</h3>
+            <p id="player__price">{`$${gameState.player2.balance}`}</p>
+          </div>
+        </div>
+      ) : ('')}
+      {gameState.player3.playing ? (
+        <div className="player-3" style={{ borderBottom: gameState.player3.turn ? '3px solid black' : '1px solid #ccc' }}>
+          <div className="player-game__details">
+            <h3 className="player-name">{`${gameState.player3.name}`}</h3>
+            <p id="player__price">{`$${gameState.player3.balance}`}</p>
+          </div>
+        </div>
+      ) : ('')}
+      {gameState.player4.playing ? (
+        <div className="player-4" style={{ borderBottom: gameState.player4.turn ? '3px solid green' : '1px solid #ccc' }}>
+          <div className="player-game__details">
+            <h3 className="player-name">{`${gameState.player4.name}`}</h3>
+            <p id="player__price">{`$${gameState.player4.balance}`}</p>
+          </div>
+        </div>
+      ) : ('')}
+    </div>
+  );
+};
+
+export default Players;
