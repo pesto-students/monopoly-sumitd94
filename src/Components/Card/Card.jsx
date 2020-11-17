9c89a7cbd2601667ba7a70278b1977a63bdca57d
Sumit Dhanania <sumitdhanania94@gmail.com>
2020-11-18 03:00:13 +0530

    Add temporary fix to useEffect warning

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -77,6 +77,7 @@ const Card = ({
     return () => {
       setShowModal(false);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [gameState.diceRolledFlag]);
 
   const modalContent = getModalContent();
